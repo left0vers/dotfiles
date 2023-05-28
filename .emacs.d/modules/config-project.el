ba8f2f91fe80e70030748678b73ae28821e4ef03
left0vers <left0vers@loudet.me>
2023-05-28 22:59:59 +0200

    emacs: switch to project.el
    `project.el' is built-in and replaces `projectile.el'

--- .emacs.d/modules/config-project.el
+++ .emacs.d/modules/config-project.el
@@ -33,5 +33,18 @@
 
 ;; (global-define-key (kbd "C-x "))
 
+
+;; /////////////////////////////////////////////////////////////////////////////
+;; project.el
+;;
+;; The following bindings mimic the ones provided by `projectile.el' that I am
+;; used to.
+(general-def 'normal
+  "SPC p p" 'project-switch-project
+  "SPC p f" 'project-find-file
+  "SPC p d" 'project-find-dir
+  "SPC p k" 'project-kill-buffers
+  "SPC p e" 'project-eshell)
+
 (provide 'config-project)
 ;;; config-project.el ends here
