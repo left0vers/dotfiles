b45bda944b0325e7b8093e1f8f74ed94e9edfb40
left0vers <left0vers@loudet.me>
2023-08-14 09:19:37 +0200

    VSCode: update install script to install extensions

--- install.sh
+++ install.sh
@@ -2,18 +2,25 @@
 
 VS_CODE_DIR=""
 
-case $(uname) in
-    'Darwin')
-    VS_CODE_DIR="${HOME}/Library/Application Support/Code/User"
-    ;;
-    'Linux')
-    VS_CODE_DIR="${HOME}/.config/Code/User/"
-    ;;
-    *)
-    echo "Possibly unsupported operating system < $(uname) >. Aborting."
-    exit
-    ;;
-esac
+if command -v code
+then
+    case $(uname) in
+        'Darwin')
+        VS_CODE_DIR="${HOME}/Library/Application Support/Code/User"
+        ;;
+        'Linux')
+        VS_CODE_DIR="${HOME}/.config/Code/User/"
+        ;;
+        *)
+        echo "Possibly unsupported operating system < $(uname) >. Aborting."
+        exit
+        ;;
+    esac
+    stow -v -d VSCode/ -t "${VS_CODE_DIR}" --ignore="txt" -S .
 
-stow --ignore="VSCode" --ignore="install.sh" -v .
-stow -d VSCode/ -t "${VS_CODE_DIR}" -S .
+    while read -r extension; do
+        code --install-extension "${extension}" --force
+    done < "${VS_CODE_DIR}/extensions.txt"
+fi
+
+stow -v --ignore="VSCode" --ignore="install.sh" -v .
